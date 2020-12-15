b177249a910874211caaba05d35b95fdba40b10b
Davi Carboni <63081313+DaviMCC@users.noreply.github.com>
2020-12-15 14:49:38 -0300

    Add files via upload

---
+++ CalculadoraDeDesconto.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+
+    float preco, precoapagar, desconto;
+    int unidades;
+
+    printf("---- Calculador de desconto -----\n");
+    printf("\nPor favor, digite o preco da unidade do produto: ");
+    scanf("%f", &preco);
+    printf("\nPor favor, digite quantas unidades serao compradas: ");
+    scanf("%d", &unidades);
+    if(unidades > 0 && unidades < 11)
+    {
+        desconto = 0;
+        precoapagar = unidades * preco;
+        printf("Total com desconto: RS%.2f", precoapagar);
+        printf("\nHouve um desconto de: RS%.2f", desconto);
+    }
+    else if(unidades > 10 && unidades < 21)
+    {
+        precoapagar = unidades * preco;
+        desconto = ((precoapagar) * 10) / 100;
+        printf("Total com desconto: RS%.2f", precoapagar);
+        printf("\nHouve um desconto de: RS%.2f", desconto);
+    }
+    else if(unidades > 21 && unidades > 50)
+    {
+        precoapagar = unidades * preco;
+        desconto = ((precoapagar) * 15) / 100;
+        printf("Total com desconto: RS%.2f", precoapagar);
+        printf("\nHouve um desconto de: RS%.2f", desconto);
+
+    }
+    else if(unidades > 50)
+    {
+        precoapagar = unidades * preco;
+        desconto = ((precoapagar) * 20) / 100;
+        printf("\nTotal com desconto: RS%.2f", precoapagar);
+        printf("Houve um desconto de: RS%.2f", desconto);
+
+    }
+    return 0;
+}
